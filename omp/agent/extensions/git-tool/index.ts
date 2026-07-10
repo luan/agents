dccba0c35b533f071b7aa9fd6a87dab781ff7d84
Luan Santos <git@luan.sh>
2026-07-10 08:44:49 -0700

    chore(agents): refresh local agent configuration

--- omp/agent/extensions/git-tool/index.ts
+++
@@ -1,8 +0,0 @@
-export { default } from "../../../../pi/agent/extensions/git-tool/index";
-export {
-	appendGitToolPrompt,
-	gitToolResources,
-	gitToolToolCallBlock,
-	parseGitToolMode,
-	type GitToolMode,
-} from "../../../../pi/agent/extensions/git-tool/index";
--- omp/agent/extensions/token-burden/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from "../../../../pi/agent/extensions/token-burden/index";
-export { buildSessionUsageData } from "../../../../pi/agent/extensions/token-burden/index";
